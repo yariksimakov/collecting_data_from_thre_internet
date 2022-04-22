ec00bbb22ecea78d542415b8b3df216fb309d666
yariksimakov <yariksimaov@mqil.ru>
2022-04-22 20:58:08 +0300

    scrapy lesson 6

---
+++ scrapy/jobparser/runner.py
@@ -0,0 +1,19 @@
+from twisted.internet import reactor
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.log import configure_logging
+from scrapy.utils.project import get_project_settings
+
+from jobparser.spiders.hhru import HhruSpider
+from jobparser.spiders.sjru import SjruSpider
+
+if __name__ == '__main__':
+    configure_logging()
+    settings = get_project_settings()
+    runner = CrawlerRunner(settings)
+    # runner.crawl(HhruSpider)
+    runner.crawl(SjruSpider)
+    #
+    # d = runner.join()
+    # d.addBoth(lambda _: reactor.stop())
+
+    reactor.run()
