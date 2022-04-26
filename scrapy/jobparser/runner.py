9adf5f62e495297ad2c8eec2007264d56431a718
yariksimakov <yariksimaov@mqil.ru>
2022-04-26 12:28:16 +0300

    seventh lesson

--- scrapy/jobparser/runner.py
+++ scrapy/jobparser/runner.py
@@ -3,16 +3,17 @@ from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
 
+from jobparser.spiders.castorama import CastoramaSpider
 from jobparser.spiders.hhru import HhruSpider
 from jobparser.spiders.sjru import SjruSpider
+from jobparser.spiders.leroymerlin import LeroymerlinSpider
 
 if __name__ == '__main__':
     configure_logging()
     settings = get_project_settings()
     runner = CrawlerRunner(settings)
-    runner.crawl(SjruSpider)
-    runner.crawl(HhruSpider)
-    d = runner.join()
-    d.addBoth(lambda _ : reactor.stop())
+    runner.crawl(CastoramaSpider, product='shower-stalls')
+    # d = runner.join()
+    # d.addBoth(lambda _ : reactor.stop())
 
     reactor.run()
